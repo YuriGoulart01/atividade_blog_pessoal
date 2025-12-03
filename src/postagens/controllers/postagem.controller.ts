6a785f991722177b7017e28eee4ad68dd7bb6f49
Yuri <yurigoulart8@gmail.com>
2025-12-03 17:58:17 -0300

    Atividades dia 2 implementadas.

--- src/postagens/controllers/postagem.controller.ts
+++ src/postagens/controllers/postagem.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get, HttpCode, HttpStatus, Param, ParseIntPipe } from "@nestjs/common";
+import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, ParseIntPipe, Post, Put } from "@nestjs/common";
 import { PostagemService } from "../services/postagem.service";
 import { Postagem } from "../entities/postagem.entity";
+import { DeleteResult } from "typeorm";
 
 @Controller('/postagens')
 export class PostagemController {
@@ -16,5 +17,29 @@ export class PostagemController {
     @HttpCode(HttpStatus.OK)
     findById(@Param('id', ParseIntPipe) id: number): Promise<Postagem>{
         return this.postagemService.findById(id);
+    }
+
+    @Get('/titulo/:titulo')
+    @HttpCode(HttpStatus.OK)
+    findByTitle(@Param('titulo') titulo:string):Promise<Postagem[]>{
+        return this.postagemService.findByTitle(titulo)
+    }
+
+    @Post()
+    @HttpCode(HttpStatus.CREATED)
+    create(@Body() postagem:Postagem): Promise<Postagem>{
+        return this.postagemService.create(postagem);
+    }
+
+    @Put()
+    @HttpCode(HttpStatus.OK)
+    update(@Body() postagem:Postagem): Promise<Postagem>{
+        return this.postagemService.update(postagem);
+    }
+    
+    @Delete('/:id')
+    @HttpCode(HttpStatus.NO_CONTENT)
+    delete(@Param('id', ParseIntPipe) id: number): Promise<DeleteResult>{
+        return this.postagemService.delete(id)
     }
 }
--- src/postagens/services/postagem.service.ts
+++ src/postagens/services/postagem.service.ts
@@ -1,7 +1,8 @@
 import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
-import { Repository } from "typeorm";
+import { ILike, Repository } from "typeorm";
 import { Postagem } from "../entities/postagem.entity";
 import { InjectRepository } from "@nestjs/typeorm";
+import { DeleteResult } from "typeorm/browser";
 
 @Injectable()
 export class PostagemService {
@@ -20,5 +21,27 @@ constructor(
         }
 
         return postagem
+    }
+
+    async findByTitle(titulo: string): Promise<Postagem[]>{
+        return await this.postagemRepository.find({
+            where:{
+                titulo: ILike(`%${titulo}`)
+            }
+        })
+    }
+
+    async create(postagem:Postagem): Promise<Postagem>{
+        return await this.postagemRepository.save(postagem);
+    }
+
+    async update(postagem: Postagem): Promise<Postagem>{
+        await this.findById(postagem.id);
+        return await this.postagemRepository.save(postagem);
+    }
+
+    async delete(id: number):Promise<DeleteResult>{
+        await this.findById(id);
+        return await this.postagemRepository.delete(id)
     }
 }
